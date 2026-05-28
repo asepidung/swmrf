582176073c299880cd1490eb193ba0f9efc38fcd
Asep Idung <asepsaidung@gmail.com>
2026-05-29 00:02:14 +0700

    installasi laravel 11, filament 3, dan login user

---
+++ lang/id/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Jalur Bahasa Paginasi
+    |--------------------------------------------------------------------------
+    |
+    | Jalur bahasa berikut digunakan oleh pustaka paginator untuk membuat
+    | tautan paginasi sederhana. Anda bebas mengubahnya menjadi apa saja
+    | yang Anda inginkan untuk menyesuaikan tampilan agar lebih cocok dengan aplikasi Anda.
+    |
+    */
+
+    'previous' => '&laquo; Sebelumnya',
+    'next' => 'Berikutnya &raquo;',
+
+];
---
+++ lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Jalur Bahasa Reset Kata Sandi
+    |--------------------------------------------------------------------------
+    |
+    | Jalur bahasa berikut adalah baris default yang cocok dengan alasan
+    | yang diberikan oleh broker kata sandi untuk upaya pembaruan kata sandi
+    | seperti kegagalan karena kata sandi / token reset yang tidak valid.
+    |
+    */
+
+    'reset' => 'Kata sandi Anda telah diatur ulang.',
+    'sent' => 'Kami telah mengirimkan tautan atur ulang kata sandi Anda.',
+    'throttled' => 'Silakan tunggu sebelum mencoba lagi.',
+    'token' => 'Token atur ulang kata sandi ini tidak valid.',
+    'user' => 'Kami tidak dapat menemukan pengguna dengan nama pengguna tersebut.',
+
+];
