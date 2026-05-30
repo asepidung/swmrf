66ce5ddef76c470469fc805c7d3eb3814cd3f783
Asep Idung <asepsaidung@gmail.com>
2026-05-30 12:22:52 +0700

    chore: remove legacy folder from git tracking and add to gitignore

--- legacy/versi prosedural/README.md
+++
@@ -1,2 +0,0 @@
-# swm
-aplikasi untuk swm
--- legacy/versi prosedural/tally/prosesistimewa.php
+++
@@ -1,6 +0,0 @@
-<?php
-require "../verifications/auth.php";
-require "../konak/conn.php";
-$idso = $_GET['idso'];
-$idtally = $_GET['idtally'];
-$idboning = $_GET['idboning'];
