869b3ae986d6fffe84ff85db5694ef59a8a79124
Asep Idung <asepsaidung@gmail.com>
2026-07-01 11:22:14 +0700

    Feat: Enable Database Notifications and 5s polling

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,6 +46,8 @@ class AdminPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Admin/Resources'), for: 'App\\Filament\\Admin\\Resources')
             ->discoverPages(in: app_path('Filament/Admin/Pages'), for: 'App\\Filament\\Admin\\Pages')
             ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters')
+            ->databaseNotifications()
+            ->databaseNotificationsPolling('5s')
             ->pages([
                 Pages\Dashboard::class,
             ])
