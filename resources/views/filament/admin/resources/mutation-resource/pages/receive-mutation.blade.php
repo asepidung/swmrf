03810bcdfe36afd99439088f23071d06c3571783
Asep Idung <asepsaidung@gmail.com>
2026-07-03 09:57:41 +0700

    Fix blade syntax error @end(php) to @endphp

--- resources/views/filament/admin/resources/mutation-resource/pages/receive-mutation.blade.php
+++ resources/views/filament/admin/resources/mutation-resource/pages/receive-mutation.blade.php
@@ -48,7 +48,7 @@
                     $totalItems = $record->items()->count();
                     $receivedItems = $record->items()->where('is_received', true)->count();
                     $waitingItems = $totalItems - $receivedItems;
-                @end(php)
+                @endphp
                 
                 <div class="grid grid-cols-2 gap-4">
                     <div class="bg-white dark:bg-gray-900 rounded-xl shadow-sm border border-gray-200 dark:border-gray-800 p-4 flex flex-col items-center justify-center relative overflow-hidden">
