0ee8d4920195f828fa351bd6d468523c97de777a
Asep Idung <asepsaidung@gmail.com>
2026-07-08 13:45:35 +0700

    fix: ensure complete alignment of NavigationGroups to eliminate sidebar duplication

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -39,18 +39,22 @@ class AdminPanelProvider extends PanelProvider
                 \Filament\Navigation\NavigationItem::make('Log Viewer')
                     ->url(fn (): string => route('log-viewer.index'))
                     ->icon('heroicon-o-document-text')
-                    ->group(__('SYSTEM'))
+                    ->group('SYSTEM')
                     ->sort(100),
             ])
             ->navigationGroups([
                 \Filament\Navigation\NavigationGroup::make('MASTER DATA')
                     ->label(__('MASTER DATA')),
+                \Filament\Navigation\NavigationGroup::make('CATTLE')
+                    ->label(__('CATTLE')),
                 \Filament\Navigation\NavigationGroup::make('REQUEST')
                     ->label(__('REQUEST')),
                 \Filament\Navigation\NavigationGroup::make('PURCHASE ORDER')
                     ->label(__('PURCHASE ORDER')),
                 \Filament\Navigation\NavigationGroup::make('GOODS RECEIPT')
                     ->label(__('GOODS RECEIPT')),
+                \Filament\Navigation\NavigationGroup::make('PRODUCTION')
+                    ->label(__('PRODUCTION')),
                 \Filament\Navigation\NavigationGroup::make('STOCKS')
                     ->label(__('STOCKS')),
                 \Filament\Navigation\NavigationGroup::make('WAREHOUSE')
@@ -59,6 +63,8 @@ class AdminPanelProvider extends PanelProvider
                     ->label(__('SALES')),
                 \Filament\Navigation\NavigationGroup::make('DISTRIBUTION')
                     ->label(__('DISTRIBUTION')),
+                \Filament\Navigation\NavigationGroup::make('FINANCE')
+                    ->label(__('FINANCE')),
                 \Filament\Navigation\NavigationGroup::make('SYSTEM')
                     ->label(__('SYSTEM')),
             ])
