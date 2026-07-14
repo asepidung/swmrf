5822e3a5e59b0ca619f558895ddb588e6ea2b880
Asep Idung <asepsaidung@gmail.com>
2026-07-14 13:49:57 +0700

    fix(log-viewer): force load published assets for modified ui

--- resources/views/vendor/log-viewer/index.blade.php
+++ resources/views/vendor/log-viewer/index.blade.php
@@ -7,20 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    @if ($assetsPublished)
-        <link rel="shortcut icon" href="{{ asset(mix('img/log-viewer-32.png', config('log-viewer.assets_path'))) }}">
-    @else
-        {!! \Opcodes\LogViewer\Facades\LogViewer::favicon() !!}
-    @endif
+    <link rel="shortcut icon" href="{{ asset('vendor/log-viewer/img/log-viewer-32.png') }}">
 
     <title>Log Viewer{{ config('app.name') ? ' - ' . config('app.name') : '' }}</title>
 
     <!-- Style sheets-->
-    @if ($assetsPublished)
-        <link href="{{ asset(mix('app.css', config('log-viewer.assets_path'))) }}" rel="stylesheet" onerror="alert('app.css failed to load. Please refresh the page, re-publish Log Viewer assets, or fix routing for vendor assets.')">
-    @else
-        {!! \Opcodes\LogViewer\Facades\LogViewer::css() !!}
-    @endif
+    <link href="{{ asset('vendor/log-viewer/app.css') }}" rel="stylesheet">
 </head>
 
 <body class="h-full px-3 lg:px-5 bg-gray-100 dark:bg-gray-900">
@@ -35,10 +27,6 @@
     // Add additional headers for LogViewer requests like so:
     // window.LogViewer.headers['Authorization'] = 'Bearer xxxxxxx';
 </script>
-@if ($assetsPublished)
-    <script src="{{ asset(mix('app.js', config('log-viewer.assets_path'))) }}" onerror="alert('app.js failed to load. Please refresh the page, re-publish Log Viewer assets, or fix routing for vendor assets.')"></script>
-@else
-    {!! \Opcodes\LogViewer\Facades\LogViewer::js() !!}
-@endif
+    <script src="{{ asset('vendor/log-viewer/app.js') }}"></script>
 </body>
 </html>
